bb3f563fce4f4c1984ebdac24e81c22fb4e1f681
murad-aliyev <98873214+murad-aliyev@users.noreply.github.com>
2022-11-29 01:42:32 +0400

    Add files via upload

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -9,33 +9,31 @@ class SubscribeButton extends Component {
             adress: "",
             phoneNumber: "",
             isValid: true,
-            pclassone: "none",
-            pclasstwo: "none"
+            name1: "none",
+            surname1: "none",
+            secondname1: "none",
+            adress1: "none",
+            phoneNumber1: "none"
+
           };
 
   handleChange = (event) => {
       this.setState({[ event.target.name] : event.target.value});
-      // if(event.target.value.length<5){
-      //   this.setState({[this.getElementById(event.target.name+"1").className] : "display"}); 
-        
-      // }else{
-      //   this.setState({[this.getElementById(event.target.name+"1").className] : "none"}); 
-
-      // }
+      if(event.target.value.length<5){
+        this.setState({[event.target.name+'1'] : "display"});
+      }else{
+        this.setState({[event.target.name+'1'] : "none"}); 
+      }
   }
 
-//   handleClick = (event) => {
-
-//     alert("Müvəffəqiyyətlə göndərildi");
-// }
-
-  handleIsValid = (event) => {
-    this.setState({ ischecked: !this.state.ischecked });
+  handleIsValid = () => {
+    {if(this.state.name1=="none" && this.state.surname1=="none" && this.state.secondname1=="none" && this.state.adress1=="none" && this.state.phoneNumber1=="none"){
+      this.setState({ isValid: false });
+    }else{
+      this.setState({ isValid: true });
+    }
   }
-
-  handleClick1 = () => this.setState({ name: ""});
-  handleClick2 = () => this.setState({ email: ""});
-
+  }
 
   handleSubmit = (evt) => {
     evt.preventDefault();
@@ -44,13 +42,12 @@ class SubscribeButton extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} className='user'>
-  <label>
-  <p>{JSON.stringify(this.state)}</p>  
+      <form onSubmit={this.handleSubmit} onChange={this.handleIsValid} className='user'>
+  <label> 
     Ad:
     <br></br>   
     <input className="user-name" id='name' name="name" onChange={this.handleChange} type="text"/>
-    <p id="name1" className={this.state.pclassone}>*Simvol sayı 4-dən çox olmalıdır</p>
+    <p id="name1" className={this.state.name1}>*Simvol sayı 4-dən çox olmalıdır</p>
   </label>
   <br></br>
   <br></br>
@@ -58,7 +55,7 @@ class SubscribeButton extends Component {
     Soyad:
     <br></br> 
     <input className="series" id="surname" name="surname" onChange={this.handleChange} type="text"/>
-    <p id="surname1" className={this.state.pclasstwo}>*Simvol sayı 4-dən çox olmalıdır</p>
+    <p id="surname1" className={this.state.surname1}>*Simvol sayı 4-dən çox olmalıdır</p>
   </label>
   <br></br>
   <br></br> 
@@ -66,6 +63,7 @@ class SubscribeButton extends Component {
     Ata adı:  
     <br></br> 
     <input className="user-age" id="secondname" name="secondname" onChange={this.handleChange} type="text"/>
+    <p id="secondname1" className={this.state.secondname1}>*Simvol sayı 4-dən çox olmalıdır</p>
   </label>
   <br></br>
   <br></br>
@@ -73,13 +71,15 @@ class SubscribeButton extends Component {
     Ünvan:
     <br></br>
     <input className="e-mail" id="adress" name="adress" onChange={this.handleChange} type="text"/>
+    <p id="adress1" className={this.state.adress1}>*Simvol sayı 4-dən çox olmalıdır</p>
   </label>
   <br></br>
   <br></br>
   <label>
    Telefon nömrəsi:
    <br></br>
-    <input className="pNumber" id="phoneNumber" name="phoneNumber" onChange={this.handleChange} type="text"/>
+    <input className="pNumber" id="phoneNumber" name="phoneNumber" onChange={this.handleChange} type="number"/>
+    <p id="phoneNumber1" className={this.state.phoneNumber1}>*Simvol sayı 4-dən çox olmalıdır</p>
   </label>
   <br></br>
   <br></br>
